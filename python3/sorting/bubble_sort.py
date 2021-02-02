5660ed27c0aefe1b295b5ebf873239e45b1c4547
Sourav Ganguly <souravganguly108@gmail.com>
2021-02-02 11:09:05 +0530

    bubble sort improved version

--- python3/sorting/bubble_sort.py
+++ python3/sorting/bubble_sort.py
@@ -9,6 +9,21 @@ def bubble_sort(elements):
                 elements[j+1] = temp
 
 
+def bubble_sort_improved_version(elements):
+    for i in range(len(elements)):
+        is_sorted = True
+        print("pass : ", i+1)
+        for j in range(len(elements)-1-i):
+            if elements[j] > elements[j+1]:
+                is_sorted = False
+                # swap
+                temp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = temp
+        if is_sorted:
+            return
+
+
 def display_elements(elements):
     for i in elements:
         print(i, end=",")
@@ -20,6 +35,8 @@ def main():
     display_elements(elements)  # displaying elements before sorting
     bubble_sort(elements)
     display_elements(elements)  # displaying elements after sorting
+
+    bubble_sort_improved_version([12,3,4,5,9])
 
 
 if __name__ == "__main__":

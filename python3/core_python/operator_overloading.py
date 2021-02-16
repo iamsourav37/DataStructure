701370dfc911cc7b2923d3dc973be217bfb3cf7c
iamsourav37 <souravganguly108@gmail.com>
2021-02-16 12:02:06 +0530

    operator overloading

--- python3/core_python/operator_overloading.py
+++ python3/core_python/operator_overloading.py
@@ -1,6 +1,3 @@
-
-
-
 class Student:
     def __init__(self, m1, m2):
         self.m1 = m1
@@ -10,12 +7,14 @@ class Student:
         return self.m1 + self.m2
 
     def __add__(self, student):
+        """ add method overloading"""
         a = self.m1 + student.m1
         b = self.m2 + student.m2
 
         return a, b
 
     def __gt__(self, other):
+        """ Greater than method overloading"""
         student1 = self.calculate_total()
         student2 = other.calculate_total()
 
@@ -35,3 +34,5 @@ if s1 > s2:
     print('s1 is greater then s2')
 else:
     print("s2 is greater than s1")
+
+print(f"Adding two student : {s1 + s2}")

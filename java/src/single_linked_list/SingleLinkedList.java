1bc53eaf048c9cc8fa268a3f5ea47f42d5d0b855
iamsourav37 <souravganguly108@gmail.com>
2021-02-28 13:59:38 +0530

    insertAt method implemented

--- java/src/single_linked_list/SingleLinkedList.java
+++ java/src/single_linked_list/SingleLinkedList.java
@@ -3,7 +3,7 @@ package single_linked_list;
 interface InsertMethods{
     void insertFirst(int data);
     void insertLast(int data);
-    void insertAt(int index, int data);
+    void insertAt(int index, int data) throws Exception;
     void insertAfter(int key, int data);
     void insertBefore(int key, int data);
 }
@@ -34,9 +34,45 @@ abstract public class SingleLinkedList implements InsertMethods,DeleteMethods {
         Node newNode = new Node(data, null);
         tmp.setNext(newNode);
     }
+    public void insertAt(int index, int data) throws Exception {
+        if(index<0 || index>this.getLength()){
+            throw new Exception("Out of range index");
+        }
+        else if(index == 0){
+            this.insertFirst(data);
+            return;
+        }else if(index == this.getLength()){
+            this.insertLast(data);
+            return;
+        }
+        int count = 0;
+        Node tmp_forward= this.head;
+        Node tmp_before = this.head;
+
+        while(tmp_forward != null){
+            if(count == index){
+                // insert the element
+                Node newNode = new Node(data, tmp_forward);
+                tmp_before.setNext(newNode);
+                return;
+            }
+
+            count++;
+            tmp_before = tmp_forward;
+            tmp_forward = tmp_forward.getNext();
+        }
+    }
 
 
-
+    public int getLength(){
+        Node node = this.head;
+        int count = 0;
+        while(node != null){
+            count ++;
+            node = node.getNext();
+        }
+        return count;
+    }
     public void showList(){
         if (this.head == null){
             System.out.println("List is empty");

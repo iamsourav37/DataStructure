e5026f9ea4a8e6b499197db73f7c8c8b071fbdc5
iamsourav37 <souravganguly108@gmail.com>
2021-03-02 10:59:35 +0530

    search and count methods added

--- java/src/binary_tree/BinaryTree.java
+++ java/src/binary_tree/BinaryTree.java
@@ -24,4 +24,51 @@ public class BinaryTree {
         return node;
     }
 
+    public int countNodes(){
+        return this.countNodes(this.root);
+    }
+    private int countNodes(TreeNode node){
+        if(node==null)
+            return 0;
+        else{
+            int count = 1;
+            count += countNodes(node.getLeft());
+            count += countNodes(node.getRight());
+            return count;
+        }
+    }
+    public int countLeaf(){
+        return countLeaf(this.root);
+    }
+    private int countLeaf(TreeNode node){
+        if (node == null)
+            return 0;
+        if (node.getLeft() == null && node.getRight() == null)
+            return 1;
+        else
+            return countLeaf(node.getLeft()) + countLeaf(node.getRight());
+    }
+
+    public boolean search(int value){
+        return this.search(root, value);
+    }
+    private boolean search(TreeNode node, int value){
+        if(node == null)
+            return false;
+        if(node.getData() == value)
+            return true;
+
+        // search in left subtree
+        boolean leftFlag = search(node.getLeft(), value);
+
+        // if flag is true then key is matching , return flag
+        if(leftFlag)
+            return true;
+
+        // if not matching then search in right subtree
+        boolean rightFlag = search(node.getRight(), value);
+
+        return rightFlag;
+    }
+
 }
